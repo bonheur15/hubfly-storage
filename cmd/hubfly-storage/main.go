cbc963493f1a3fbc8cb19d83fcdee6335c5465fa
bonheur15 <rumanzibonheur@gmail.com>
2025-11-19 16:09:01 +0200

    REFACTOR: Rename /volumes/stats to /dev/volumes and remove GetVolumesStatsHandler

--- cmd/hubfly-storage/main.go
+++ cmd/hubfly-storage/main.go
@@ -17,9 +17,8 @@ func main() {
 	http.HandleFunc("/create-volume", handlers.CreateVolumeHandler(baseDir))
 	http.HandleFunc("/delete-volume", handlers.DeleteVolumeHandler(baseDir))
 	http.HandleFunc("/health", handlers.HealthCheckHandler())
-	        http.HandleFunc("/volume-stats", handlers.GetVolumeStatsHandler(baseDir))
-	        http.HandleFunc("/volumes", handlers.GetVolumesHandler(baseDir))
-        http.HandleFunc("/volumes/stats", handlers.GetVolumesStatsHandler(baseDir))
+	http.HandleFunc("/volume-stats", handlers.GetVolumeStatsHandler(baseDir))
+	http.HandleFunc("/dev/volumes", handlers.GetVolumesHandler(baseDir))
 	log.Println("🚀 Server running on port 8203...")
 	if err := http.ListenAndServe(":8203", nil); err != nil {
 		log.Fatalf("Server error: %v", err)
